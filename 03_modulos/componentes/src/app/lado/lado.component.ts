a55f37eefb6deae1c592862034f5708f2ea78e74
JoseCenteno <joseluiscenteno@gmail.com>
2017-10-03 13:51:49 +0200

    Modulos

---
+++ 03_modulos/componentes/src/app/lado/lado.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-lado',
+    template: '<div> esto va a un lado</div>'
+})
+
+export class LadoComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
